b65e7704d2e17602d4d5fdf065285f84db9e523c
Dicki Fatma <dickifatma@Macbook.local>
2024-06-20 02:45:08 +0700

    First Version

---
+++ README.md
@@ -0,0 +1,42 @@
+# SONIC TX MILESTONE BOT
+Sonic TX Milestone Bot is a tool for sending solana ($SOL) tokens to another address whos generate on script for completing task: 100 Transaction Daily.
+
+## USAGE
+1. Clone the repository:
+    ```
+    git clone https://github.com/dickifatma/sonic-tx-bot.git
+    ```
+2. Install dependencies:
+    ```
+    cd sonic-tx-bot
+    npm install
+    ```
+3. Prepare your `privkey` address solana from solflare or phantom or etc
+
+4. Run bot:
+    ```
+    node index.js
+    ```
+
+## Configuration
+
+### Privkey
+Example: 
+    ```
+    [22,22,22,22,22,22,22,22,22,22,22,22,22]
+    ```
+
+## Set Total Transaction
+Example: 
+    ```
+    const txCount = 1 // Edit "1" with number you want
+    ```
+
+# KEY FEATURE
+* Auto generate address
+* Custom total transaction
+
+# WILL UPDATE
+- [] Sleep Timer
+- [] Support Multi Account
+- [] Support Phrase
---
+++ index.js
@@ -0,0 +1,29 @@
+const web3 = require("@solana/web3.js");
+const chalk = require("chalk");
+
+const connection = new web3.Connection("https://devnet.sonic.game", 'confirmed') // Tempat mengganti rpc apabila kedepannya diperlukan
+
+const privkey = [0o0,... 0o0]; // Ubah dengan privkey anda
+const from = web3.Keypair.fromSecretKey(new Uint8Array(privkey))
+const to = web3.Keypair.generate(); // Auto generator address SOL
+
+(async () => {
+    const transaction = new web3.Transaction().add(
+        web3.SystemProgram.transfer({
+          fromPubkey: from.publicKey,
+          toPubkey: to.publicKey,
+          lamports: web3.LAMPORTS_PER_SOL / 100,
+        }),
+      );
+    
+      // MAIN TX
+      const txCount = 1; // Ubah angka sesuai dengan jumlah transaksi yang anda inginkan
+      for (let i = 0; i < txCount; i++) {
+      const signature = await web3.sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [from],
+      );
+      console.log(chalk.blue('Create tx done, tx hash ->'), signature);
+    }
+})()
